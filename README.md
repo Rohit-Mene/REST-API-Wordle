cb901bb1431bf66458799280d574fc5b78bee9b8
sean <spmccarthy4@gmail.com>
2022-10-21 21:06:17 -0700

    working games and guess functionality

--- README.md
+++ README.md
@@ -18,44 +18,72 @@ API DOCUMENTATION-
 3) Start a game:
   #http POST http://localhost:5000/startgame/<user_id>
 
-#http GET http://127.0.0.1:5000/games/<:id> returns dictionary of all active games by a single user with id <:id>
+4)Retrieve a list of all active games for a plater
+  #http GET http://127.0.0.1:5000/games/<:id> 
+  
+  Returns dictionary of all active games by a single user with id <:id>
+    Sample Output:
+        {
+            "game_id": 4
+        },
+        {
+            "game_id": 8
+        },
+        {
+            "game_id": 13
+        }
+            Output Format.
+                game_id represents a game that is active for the player in question.
 
-#http GET http://localhost:5000/gamestate/ game:='{"game_id":1}'
+5) #http GET http://localhost:5000/gamestate/ game:='{"game_id":1}'
 
-http --form PUT http://127.0.0.1:5000/guess/ game_id=8 guess="kneed"
-If the word is the correct word it returns a JSON object in the form of,        
+6) Make a guess in an active game:
+  http PUT http://127.0.0.1:5000/guess/ guess_to_make:='{"game_id":"#","guess":"     "}'
+  Use the JSON format after URL to enter input data for this api. Enter the game ID where # is and enter guess word in blank string.
+    
+    6a) If the word is the correct word it returns a JSON object in the form of,        
 
-    {correct_word: TRUE}
+    Sample Output:
+        {
+        "correct_word": "TRUE"
+        }   
+            Output format.
+                correct_word states if the word entered is the secret word for the specified game.
 
-If the word is valid but not correct it returns a JSON object in the form of, 
+    6b) If the word is valid but not correct it returns a JSON object in the form of, 
 
-    "{valid :TRUE ,  guess_remaining :2, correct position :, correct letter incorrect spot :1}"
+    Sample Output:
+        {
+        "correct_letter_incorrect_spot ": "",
+        "correct_position": "",
+        "guess_remaining ": "",
+        "valid": "TRUE"
+        }
+            Output format.
+                valid states if a word entered is a valid guess
+                guess_remaining indicates the number of guesses left in the game before it ends.
+                correct_position is an array containing the index's of letters in the correct position. 
+                correct_letter_incorrect_spot is an array of the index's of letters that are in the guess word but in the wrong position in the guess word. 
 
-correct position is an array containing the index's of letters in the correct position, correct letter incorrect spot is an array of the index's of letters that 
-are in the guess word but in the wrong position in the guess word. 
+    6c) If you run out of guesses without guessing the correct word you return a JSON object in the form of 
 
-If you run out of guesses without guessing the correct word you return a JSON object in the form of 
+    Sample Output:
+        {
+        "game_sts": "TRUE",
+        "guess_rem": "0"
+        }
+            Output Format.
+                Game_sts is false when the game is still active and true when game is no longer active.
 
-       "{guess_rem : 0, game_sts: TRUE}"
+    6d) If the guess is invalid it returns a JSON object in the form of 
 
-Game_sts is false when the game is still active and true when game is no longer active
+    Sample Output:
+        {
+        "guess_rem": "#",
+        "valid": "FALSE"
+        }
+            Output Format.
+                valid represents if the word guessed is a valid guess.
+                guess_rem represents the number of guesses remaining.
 
-If the guess is invalid it returns a JSON object in the form of 
-
-    {valid: FALSE, guess_rem : # }"
-
-
-http GET http://127.0.0.1:5000/games/6
-
-This returns all games currently active by user (in this case 6) it returns a JSON object in the form of 
-
-     {
-        "game_id": 4
-    },
-    {
-        "game_id": 8
-    },
-    {
-        "game_id": 13
-    }
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import dataclasses
 import json
+import logging
 import random
 import sqlite3
 from quart import Quart,g,request,abort,Response
@@ -142,53 +143,78 @@ async def gamestate():
 
 
 @app.route("/guess/", methods=["PUT"])
-#@validate_request(Guess)
 async def make_guess():
+    #contact db
     db = await _get_db()
+    #retrieve json
     data = await request.get_json()
-    guess_made={'game_id': data.get('guess_to_make').get('game_id'),'guess': data.get('guess_to_make').get('guess')}
-    file = open('valid.json')
-    word_list = json.load(file)
-    #obtain secret word
+    #store game id input
+    guess_id={'game_id': data.get('guess_to_make').get('game_id')}
+    #store word guessed
+    string_guess = {'guess': data.get('guess_to_make').get('guess')}
+    #check to see if word entered is a valid guess
     try:
-        gueses_left = await db.fetch_val(
+        valid_check = await db.fetch_val(
             """
-            SELECT guess_cnt FROM USERGAMEDATA WHERE game_id = :game_id
-            """,
-            guess_made,
+                SELECT word_id FROM VALIDWORD WHERE EXISTS(SELECT word_id FROM VALIDWORD WHERE valid_word = :guess)
+            """, 
+            string_guess,
         )
     except sqlite3.IntegrityError as e:
         abort(500, e)
+    #check to see if word entered is not on valid list
+    try:
+        invalid_check = await db.fetch_val(
+            """
+                SELECT * FROM VALIDWORD WHERE NOT EXISTS(SELECT word_id FROM VALIDWORD WHERE valid_word = :guess)
+            """, 
+            string_guess,
+        )
+    except sqlite3.IntegrityError as e:
+        abort(500, e)
+    #obtain number of guesses left
+    try:
+        gueses_left = await db.fetch_val(
+            """
+                SELECT guess_cnt FROM USERGAMEDATA WHERE game_id = :game_id
+            """,
+            guess_id,
+        )     
+    except sqlite3.IntegrityError as e:
+        abort(500, e)
+    #determine if the game is an active game
     try:
         completed_game = await db.fetch_val(
             """
             SELECT game_sts FROM USERGAMEDATA WHERE game_id = :game_id
             """,
-            guess_made,
+            guess_id,
         )
     except sqlite3.IntegrityError as e:
         abort(500, e)    
+    #obtain secret word
     try:
         secret_word = await db.fetch_val(
             """
             SELECT secret_word FROM USERGAMEDATA WHERE game_id = :game_id
             """,
-            guess_made,
+            guess_id,
         )
-        #test if guess is correct
-        if data[':guess'] == secret_word and completed_game == False:
+        #test if guess is secret word, and make sure game is active
+        if data.get('guess_to_make').get('guess') == secret_word and completed_game == False:
             try:
                 #if correct word decrease guess remaining and change game state to false
                 await db.execute(
                     """ 
                         UPDATE USERGAMEDATA SET guess_cnt = guess_cnt - 1, game_sts = TRUE WHERE game_id = :game_id;
                     """,
-                    guess_made
+                    guess_id
                 )
             except sqlite3.IntegrityError as e:
                 abort(500, e)
+            #insert relevant data from guess into the guess table to track the valid guess made
             try:
-                insert_tuple = {'game_id':data['game_id'], 'guess_num' :(6 - gueses_left + 1), 'guessed_word':data['guess']}
+                insert_tuple = {'game_id':data.get('guess_to_make').get('game_id'), 'guess_num' :(6 - gueses_left + 1), 'guessed_word':data.get('guess_to_make').get('guess')}
                 await db.execute(
                     """ 
                         INSERT INTO guess(game_id, guess_num, guessed_word) VALUES(:game_id, :guess_num, :guessed_word)                        
@@ -197,21 +223,22 @@ async def make_guess():
             except sqlite3.IntegrityError as e:
                 abort(500, e)
             #if correct return 
-            return Response(json.dumps("{correct_word: TRUE}"),status=201)
+            return {"correct_word": "TRUE"},201
         #if guess is not correct but valid 
-        elif data[':guess'] in word_list and completed_game == False and gueses_left > 1:
+        elif valid_check and completed_game == False and gueses_left > 1:
             try:
                 #decrease guesses remaining
                 await db.execute(
                     """ 
                         UPDATE USERGAMEDATA SET guess_cnt = guess_cnt - 1 WHERE game_id = :game_id;
                     """,
-                    guess_made
+                    guess_id
                 )
             except sqlite3.IntegrityError as e:
                 abort(500, e)
+            #insert the valid guess into the guess table to track the guess
             try:
-                insert_tuple = {'game_id':data['game_id'], 'guess_num' :6 - gueses_left, 'guessed_word':data['guess']}
+                insert_tuple = {'game_id':data.get('guess_to_make').get('game_id'), 'guess_num' :6 - gueses_left, 'guessed_word':data.get('guess_to_make').get('guess')}
                 await db.execute(
                     """ 
                         INSERT INTO guess(game_id, guess_num, guessed_word) VALUES(:game_id, :guess_num, :guessed_word)                        
@@ -219,8 +246,9 @@ async def make_guess():
                 )
             except sqlite3.IntegrityError as e:
                 abort(500, e)
-            #obtain new guess count to return
-            guess_word = str(data[':guess'])
+            #obtain new guess count for return data
+            guess_word = str(data.get('guess_to_make').get('guess'))
+            #the next section is the logic to obtain what letters are in the correct spot
             #new lists to store letter positions
             correct_spot_list = []
             correct_letter_list = []
@@ -241,33 +269,36 @@ async def make_guess():
             #nested for loop to remove duplicate values from the two lists
             spot_to_string = ' '.join(map(str,spot))
             letter_to_string = ' '.join(map(str,letter))
-            return Response(json.dumps('{valid :TRUE ,  guess_remaining :' + str(gueses_left - 1) + ', correct position :' + spot_to_string + ', correct letter incorrect spot :' + letter_to_string + '}'),status=201)
-        #if guess is not on valid list tell the user to try a different word
+            return {"valid":"TRUE" ,  "guess_remaining ": str(gueses_left - 1), "correct_position" : spot_to_string , "correct_letter_incorrect_spot ": letter_to_string},201
+        #if no guesses remian in game
         elif gueses_left <= 1:
             try:
-                #no guesses left 
+                #change game state to TRUE meaning game is over 
                 await db.execute(
                     """ 
                         UPDATE USERGAMEDATA SET game_sts = TRUE WHERE game_id = :game_id;
                     """,
-                    guess_made
+                    guess_id
                 )
             except sqlite3.IntegrityError as e:
                 abort(500, e)
-            return Response(json.dumps("{guess_rem : 0, game_sts: TRUE}"),status=201)
-        else:
-            return Response(json.dumps("{valid: FALSE, guess_rem :"+ str(gueses_left) + "}"),status=204) 
+            #return statement letting player know they are out of guesses    
+            return {"guess_rem" : "0","game_sts": "TRUE"},201
+        #if the word guessed is invalid let the user know it is not valid and they must guess again
+        elif invalid_check:
+            return {"valid": "FALSE", "guess_rem" : str(gueses_left)},201 
         
     except sqlite3.IntegrityError as e:
         abort(409, e)
 
-
 @app.route("/games/<int:user_id>", methods=["GET"])
 async def all_games(user_id):
+    #connect to db
     db = await _get_db()
+    #select all active games for a single user
     game = await db.fetch_all("select game_id from USERGAMEDATA where user_id = :user_id AND game_sts = FALSE", values={"user_id":user_id})
     if game:
-        return Response(json.dumps(list(map(dict,game))), status=201)
+        return list(map(dict,game)),201
     else:
         abort(404)
 
