f128d8ca0a82ff63a904e9c16316280364d54c48
rohitmene <rohitmene@gmail.com>
2022-12-03 01:22:30 -0500

    fix init file

--- bin/init.sh
+++ bin/init.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-echo "Creating sqlite databases"
-sqlite3 var/user.db < share/user.sql
-sqlite3 var/primary/mount/game.db < share/game.sql
-
 echo "Creating data and mount folders"
 mkdir -p var/primary/mount
 mkdir -p var/secondary1/mount
@@ -12,5 +8,11 @@ mkdir -p var/secondary2/mount
 mkdir -p var/primary/data
 mkdir -p var/secondary1/data
 mkdir -p var/secondary2/data
+
+echo "Creating sqlite databases"
+sqlite3 var/user.db < share/user.sql
+sqlite3 var/primary/mount/game.db < share/game.sql
+
+
 
 
